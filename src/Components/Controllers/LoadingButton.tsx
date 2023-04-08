b60a4b7ce408de3292aa8e03b2216231cbf1fc24
Adnane Aref <adnane.jr2021@gmail.com>
2023-04-09 01:05:57 +0200

    Feat: SmartLoading button

--- src/Components/Controllers/LoadingButton.tsx
+++ src/Components/Controllers/LoadingButton.tsx
@@ -1,5 +1,12 @@
-import { Button, ButtonProps, CircularProgress, styled } from '@mui/material';
-import React, { useCallback } from 'react';
+import {
+  Button,
+  ButtonProps,
+  CircularProgress,
+  IconButton,
+  IconButtonProps,
+  styled,
+} from '@mui/material';
+import { useCallback } from 'react';
 
 const StyledButton = styled(Button)(() => ({
   '.MuiCircularProgress-root': {
@@ -9,25 +16,60 @@ const StyledButton = styled(Button)(() => ({
   },
 }));
 
-interface LoadingButtonProps extends ButtonProps {
+const StyledIconButton = styled(IconButton)(() => ({
+  '.MuiCircularProgress-root': {
+    width: '1em !important',
+    height: '1em !important',
+    color: 'currentColor',
+  },
+}));
+
+type Types = 'IconButton' | 'Button';
+type ConditionalExtend<T extends Types> = Omit<
+  T extends 'IconButton' ? IconButtonProps : ButtonProps,
+  'color'
+>;
+
+interface LoadingButtonProps {
   loading?: boolean;
+  buttonType?: Types;
+  color?: ButtonProps['color'];
 }
 
-export const LoadingButton: React.FC<LoadingButtonProps> = ({
-  loading,
-  startIcon,
+export const LoadingButton = <T extends Types>({
+  loading = false,
   children,
+  buttonType = 'Button',
   ...props
-}) => {
+}: LoadingButtonProps & ConditionalExtend<T>) => {
   const disabled = Boolean(loading || props.disabled);
 
   const renderIcon = useCallback(() => {
-    return loading ? <CircularProgress variant='indeterminate' /> : startIcon;
-  }, [loading, startIcon]);
+    if (!buttonType) return children;
 
-  return (
+    switch (buttonType) {
+      case 'Button':
+        return loading ? (
+          <CircularProgress variant='indeterminate' />
+        ) : (
+          (props as ButtonProps).startIcon
+        );
+
+      case 'IconButton':
+        return loading ? <CircularProgress variant='indeterminate' /> : children;
+
+      default:
+        return children;
+    }
+  }, [loading, props]);
+
+  return buttonType === 'Button' ? (
     <StyledButton disabled={disabled} startIcon={renderIcon()} {...props}>
       {children}
     </StyledButton>
+  ) : (
+    <StyledIconButton disabled={disabled} {...props}>
+      {renderIcon()}
+    </StyledIconButton>
   );
 };
