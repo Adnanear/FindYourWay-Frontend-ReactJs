3ba42877cf5d121fecaadac20958a2a7eb4d07bf
Adnane Aref <adnane.jr2021@gmail.com>
2023-04-08 22:26:39 +0200

    Feat: Table component accepts actions

--- src/Components/DataGrid/Table.tsx
+++ src/Components/DataGrid/Table.tsx
@@ -7,7 +7,9 @@ import TableRow from '@mui/material/TableRow';
 import { useCallback } from 'react';
 import { PaperContainer } from '../Containers';
 
-interface TableProps<T extends object[]> {
+type ObjectType = object[] | readonly object[];
+
+interface TableProps<T extends ObjectType> {
   data: T;
   cells: Partial<{
     [key in keyof T[number]]: string;
@@ -17,8 +19,14 @@ interface TableProps<T extends object[]> {
       | ((value: TableProps<T>['data'][number][key]) => React.ReactNode)
       | string;
   }>;
+  actions?: (row: TableProps<T>['data'][number]) => React.ReactNode;
 }
-export const Table = <T extends object[]>({ data, cells, renderValues }: TableProps<T>) => {
+export const Table = <T extends ObjectType>({
+  data,
+  cells,
+  renderValues,
+  actions,
+}: TableProps<T>) => {
   const renderValueOrDefault = useCallback(
     (row: typeof data[number], key: string | number | symbol) => {
       const defaultValue = row[key as keyof typeof row];
@@ -36,29 +44,41 @@ export const Table = <T extends object[]>({ data, cells, renderValues }: TablePr
     [renderValues, data, cells],
   );
 
+  const renderRowActions = useCallback(
+    (row: typeof data[number]) => {
+      return actions?.(row);
+    },
+    [actions, data],
+  );
+
   return (
     <TableContainer component={PaperContainer} sx={{ p: 0 }}>
-      <DefaultTable size='small'>
+      <DefaultTable
+        size='small'
+        sx={{
+          '& th, & td': {
+            borderBottom: '1px solid',
+            borderBottomColor: 'divider',
+          },
+        }}
+      >
         <TableHead>
           <TableRow>
             {Object.entries(cells).map(([key, replacement]) => (
-              <TableCell key={key} sx={{ borderBottom: '1px solid', borderBottomColor: 'divider' }}>
-                {String(replacement)}
-              </TableCell>
+              <TableCell key={key}>{String(replacement)}</TableCell>
             ))}
+            <TableCell />
           </TableRow>
         </TableHead>
         <TableBody>
           {data.map((x, idx) => (
             <TableRow key={idx}>
               {Object.entries(cells).map(([cellKey]) => (
-                <TableCell
-                  key={`${idx}-${String(cellKey)}`}
-                  sx={{ borderBottom: '1px solid', borderBottomColor: 'divider' }}
-                >
+                <TableCell key={`${idx}-${String(cellKey)}`}>
                   {renderValueOrDefault(x, cellKey)}
                 </TableCell>
               ))}
+              <TableCell>{renderRowActions(x)}</TableCell>
             </TableRow>
           ))}
         </TableBody>
