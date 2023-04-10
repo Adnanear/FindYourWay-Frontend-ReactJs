cc00d4f3fac811ff543f5b312c648ed917a33f32
Adnane Aref <adnane.jr2021@gmail.com>
2023-04-11 00:58:23 +0200

    Fix: Authorization issues

--- src/Utils/Request.ts
+++ src/Utils/Request.ts
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    // Authorization: `Bearer <token-id>`,
-    'Content-Type': 'application/json',
-  },
 });
 
 export default request;
+
+request.interceptors.request.use((config) => {
+  config.headers.Authorization = localStorage.getItem(
+    `${import.meta.env.VITE_LOCAL_STORAGE_PREFIX}_token`,
+  );
+
+  return config;
+});
